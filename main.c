7e1371f4c5d41e58e761d6f1ce80f81ac0fccce1
Sandro <81508801+Sandroid75@users.noreply.github.com>
2021-12-19 19:14:06 +0100

    Create main.c
    First version

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *numberTOwords(long number);
+
+int main() {
+    //Get input number from user
+    long num;
+    char *str;
+    
+    // convert given number in words
+    while(1) {
+        printf("Number: ");
+        scanf("%ld", &num);
+        if(num < 0) {
+            break;
+        }
+        str = numberTOwords(num);
+        printf("Words:\t%s\n", str);
+        free(str);
+    }
+    
+    return 0;
+}
